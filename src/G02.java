5eff4e6cf315e521fb16964ac250a5f86a837897
suleymanunal <125310774+suleyman4332@users.noreply.github.com>
2023-03-29 17:16:43 +0300

    4.gün

---
+++ src/G02.java
@@ -0,0 +1,17 @@
+public class G02 {
+    /*
+    git git up da projelerimizin üzerinde çalısırken temelde 3 adımda işlem yapıyoruz
+    1-projelerimizi yada dosyalarımızı git ile ilişkilendiriyoruz
+    git init
+    2-projemizde /dosyamızda bulunanları versiyon oluşturuyorduk
+    git add .
+    git commit -m "versiyonadı"
+    3-git up a yollama entegrasyonu
+
+    git remote add origin repoUrlAdresi
+    already origin
+    git remote set-url repoUrlAdresi
+    git push
+
+     */
+}
